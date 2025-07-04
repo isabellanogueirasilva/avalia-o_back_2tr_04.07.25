62a012241e972880850a9a437e586e1363443a0b
isabellanogueirasilva <159037643+isabellanogueirasilva@users.noreply.github.com>
2025-07-04 11:15:36 -0300

    Create perfil_pessoal_tb.py

---
+++ perfil_pessoal_tb.py
@@ -0,0 +1,14 @@
+def perfil_pessoal():
+    print("\n Perfil Pessoal")
+    ocupacao = input("Ocupação: ")
+    idade = input("Idade: ")
+    estado_civil = input("Estado Civil: ")
+    objetivo = input("Objetivo: ")
+
+    print("\nDados do Perfil Pessoal")
+    print(f"Ocupação: {ocupacao}")
+    print(f"Idade: {idade}")
+    print(f"Estado Civil: {estado_civil}")
+    print(f"Objetivo: {objetivo}")
+
+perfil_pessoal()
